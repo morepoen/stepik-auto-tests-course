836038df65a8d5d4e58bea81c8f28434d1160e9d
Moomini <anna191110@gmail.com>
2019-12-18 17:36:15 +0300

    better version

--- test_items.py
+++ test_items.py
@@ -1,10 +1,16 @@
+import time
 
 link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 
 def test_is_button_present(browser):
     browser.get(link)
-    assert browser.find_element_by_css_selector(".btn-add-to-basket"), "No such button"
+    add_to_basket_button = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
+    assert add_to_basket_button > 0, "Button add-to-basket does not exist"
+
+
+
+
 
 
 
