e446131960b62281a528e60da7f3f87249771f9f
Moomini <anna191110@gmail.com>
2019-12-18 14:53:29 +0300

    modified test_items

--- test_items.py
+++ test_items.py
@@ -1,13 +1,12 @@
-from selenium.common.exceptions import NoSuchElementException
 
 link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 
 def test_is_button_present(browser):
+    browser.get(link)
+    assert browser.find_element_by_css_selector(".btn-add-to-basket"), "No such button"
 
-    try:
-        browser.get(link)
-        browser.find_element_by_css_selector(".btn-add-to-basket")
-    except NoSuchElementException:
-        return False
-    return True
+
+
+
+
