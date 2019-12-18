1c7b4ee6128f935ba4e019bbea9760837d22bf9b
Moomini <anna191110@gmail.com>
2019-12-18 22:20:10 +0300

    better version

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 import time
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_is_button_present(browser):
